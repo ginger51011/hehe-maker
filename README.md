fc92431b2759120be3b4a96a3ff962f819c55bc2
Emil Jonathan Eriksson <eje1999@gmail.com>
2019-12-18 22:42:00 +0100

    Added -rm functionality

--- README.md
+++ README.md
@@ -21,9 +21,12 @@ Följ installationsguiden ovan. När du gjort det, ladda ner detta projektet. Ö
 ### Flaggor
 Flaggor kan användas för att ändra vad HeHE-maker gör. Nedan följer en lista. Bara skriv in efter `python hehemaker.py`!
 
+* Är denna listan inte hjälpsam nog? Skriv `-h` eller `--help`!
 * Man kan förtrycka kravet på att man ska kunna trycka allt på ett jämnt antal uppslag genom att lägga till flaggan `-f` eller `--force` före `input`.
 * Skulle du istället vilja konvertera från en printversion till en webbversion, använd `-s` eller `--split` före `input` (det är ej nödvändigt att använda `-f` samtidigt).
 * Har du en webbversion (alla sidor efter varandra i ordning i en enda PDF-fil) kan du använda `-wtp` eller `--web-to-print` för att göra om till en tryckbar version.
+* Vill du ta bort en eller flera sidor kan du använda `-rm` eller `--remove`, följt av sidnummret eller sidnummerna.
+** Exempel: Du vill ta bort första och sista sidan. Då skriver du helt enkelt in `python hehemaker.py -rm 1 12 "<sökväg till input>" "<sökväg till output>"`!
 
 ## Varför
 Att sitta och göra denna tidningen i PowerPoint som Redaktionen '19 är inte optimalt.
--- hehemaker.py
+++ hehemaker.py
@@ -5,23 +5,25 @@ import argparse
 
 # Parser so we can control everything from the command line (smaht)
 parser = argparse.ArgumentParser()
-parser.add_argument("input", help="Path to pages")  # Adds parameter to parser
-parser.add_argument("output", help="Path to where you want the papers saved")
 parser.add_argument("-f", "--force", action="store_true", help="Suppresses the need for the number of pages to be = 0 (mod 4)")     # If flag is used saves a true value
 parser.add_argument("-s", "--split", action="store_true", help="Will split pages in two, ordering as if this was a print file")     # Flag for splitting a print version
 parser.add_argument("-wtp", "--web-to-print", action="store_true", help="Will create a printable version of a web PDF")     # Seems to convert - to _ for variable name
+parser.add_argument("-rm", "--remove", action="store_const", const=int, default=[], help="Removes the pages given from the PDF specified")      # Flag to remove numbers
+parser.add_argument("rm_pages", nargs="*", metavar="P", type=int, help="A page to be removed")      # Pages to be removed as specified by user, nargs="*" used to set as optional and a list
+parser.add_argument("input", help="Path to pages")  # Adds parameter to parser
+parser.add_argument("output", help="Path to where you want the papers saved")
 args = parser.parse_args()      # Collects our input in args
 
 # Throws an exception if we don't force mod 4 != 0
 page_listings = os.listdir(args.input)      # Returns a list with the files in the directory
-if not (args.force or args.split or args.web_to_print):
+if not (args.force or args.split or args.web_to_print or args.remove):
     nbr_of_pages = len(page_listings)
     if (nbr_of_pages % 4 != 0):     # If we don't have mod 4 == 0 we can't create a paper
         raise ValueError("Number of pages does not give mod 4 == 0; Then you can't create a (nice) paper version")
 
 pages_in = []
 # We will need more than one reader if we are putting different files together
-if not (args.split or args.web_to_print):
+if not (args.split or args.web_to_print or args.remove):
     pdf_readers = []
 
     # Puts out a PdfReader for each PDF document (page)
@@ -106,11 +108,20 @@ def splitpage(page):
         # We return (yield) a generator, which in turn generates a collection later
         yield PageMerge().add(page, viewrect=(x, 0, 0.5, 1)).render()
 
+# Removes pages specified
+def remove_pages(pages_in, page_numbers):
+    pages_out = pages_in.copy()
+    for n in page_numbers:
+        del pages_out[n - 1]     # Our list index start at 0, but the user starts counting pages at 1
+    PdfWriter(args.output + "\\removed.pdf").addpages(pages_out).write()
+
 # Runs our program
 if args.split:
     print_to_web(pages_in)
 elif args.web_to_print:
     create_print_version(pages_in)
+elif args.remove:
+    remove_pages(pages_in, args.rm_pages)
 else:
     create_print_version(pages_in)
     create_web_version(pages_in)
