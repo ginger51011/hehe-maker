5e02cac50b9948761d3984cacde24b3ff6fd2fcf
Emil J Eriksson <eje1999@gmail.com>
2019-12-23 20:26:07 +0100

    Preparation for PyPi launch

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 # Denna fil är endast till för att det ska gå att installera hehemaker.py.
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_descrtiption = fh.read()
+
 setup(
     name='hehe-maker',
-    version='1.0.1a',
+    version='1.1.1',
     description='Ett enkelt kommandoverktyg för att sätta ihop PDF:er till HeHE',
     author='Emil Jonathan Eriksson',
     author_email='eje1999@gmail.com',
