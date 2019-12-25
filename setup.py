2e0815305939848db53a1227f75191f29cb47e4d
Emil J Eriksson <eje1999@gmail.com>
2019-12-25 14:53:49 +0100

    Fixed version numbering

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hehe-maker',
-    version='1.1.3',
+    version='1.1.4',
     author='Emil Jonathan Eriksson',
     author_email='eje1999@gmail.com',
     description='Ett enkelt kommandoverktyg för att sätta ihop PDF:er till HeHE | A simple command line tool to modify papers in PDF format',
